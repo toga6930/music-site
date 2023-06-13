d22c20c4f818b0c42b977a618c9ec21044754637
LeaDerBone <hhh6930@gmail.com>
2023-06-13 19:48:58 +0900

    1st ver.

--- app/components/Likes.tsx
+++ app/components/Likes.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { SongInt } from "../types/types";
+import Image from "next/image";
 
 const Likes = () => {
     const [liked, setLiked] = useState<SongInt[]>([]);
@@ -10,11 +11,11 @@ const Likes = () => {
           setLiked(parsedLikedSongs);
         }
       }, []);
-      const handleDelete = (songId: number) => {
+    const handleDelete = (songId: number) => {
         const updatedLikedSongs = liked.filter((song) => song.id !== songId);
         setLiked(updatedLikedSongs);
         localStorage.setItem("likedSongs", JSON.stringify(updatedLikedSongs));
-      };
+    };
     return (
         <>
             <div className="flex cursor-pointer w-full min-h-screen justify-center pt-16 pb-20 dark:bg-slate-800">
@@ -23,7 +24,7 @@ const Likes = () => {
                         <li key={song.id} 
                             className="flex flex-col md:flex-row items-center space-y-2 md:space-y-0 space-x-2 md:space-x-8 border-4 rounded-lg mt-2 md:w-full border-pink-200 hover:bg-pink-200 dark:hover:bg-slate-900 dark:text-pink-100 shadow-lg">
                             <div className="flex-shrink-0">
-                                <img className="w-28 h-28 p-2 md:mx-auto" src={song.img_medium} alt="cover"/>
+                                <Image className="w-28 h-28 p-2 md:mx-auto" src={song.img_medium} alt="cover"/>
                             </div>
                             <div className="flex-1">
                                 <p className="flex text-center truncate md:m-auto text-2xl">
